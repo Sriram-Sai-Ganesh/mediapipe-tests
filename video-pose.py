c5b718abcef4e672eb7c4a3cf19ffa30d5e7f287
ram <35802333+Sriram-Sai-Ganesh@users.noreply.github.com>
2023-05-31 12:58:01 +0530

    working on function to calculate spine angle

--- video-pose.py
+++ video-pose.py
@@ -5,12 +5,21 @@ from mediapipe.tasks.python import vision
 import cv2
 from datetime import datetime
 
-#drawing
 from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
 import numpy as np
 
 
+# objects
+BaseOptions = mp.tasks.BaseOptions
+PoseLandmarker = mp.tasks.vision.PoseLandmarker
+PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
+PoseLandmarkerResult = mp.tasks.vision.PoseLandmarkerResult
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+# function to draw landmarks on video feed per-frame
+# receives point locations from PoseLandmarker model
+# draws points using the mediapipe solutions' drawingutils
 def draw_landmarks_on_image(rgb_image, detection_result):
 	pose_landmarks_list = detection_result.pose_landmarks
 	annotated_image = np.copy(rgb_image)
@@ -31,65 +40,63 @@ def draw_landmarks_on_image(rgb_image, detection_result):
 			solutions.drawing_styles.get_default_pose_landmarks_style())
 	return annotated_image
 
-
-
-model_path = 'pose_landmarker_heavy.task'
-
-
-BaseOptions = mp.tasks.BaseOptions
-PoseLandmarker = mp.tasks.vision.PoseLandmarker
-PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
-PoseLandmarkerResult = mp.tasks.vision.PoseLandmarkerResult
-VisionRunningMode = mp.tasks.vision.RunningMode
-
-
-# Create a pose landmarker instance with the live stream mode:
-def print_result(result: PoseLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
-	# annotated_image = draw_landmarks_on_image(mp_image, result)
-	# cv2.imshow(cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
+# function to print PoseLandmarkerResult object
+def print_result(result: mp.tasks.vision.PoseLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
 	print('pose landmarker result: {}'.format(result))
 
+# calculate angle between horizontal and individual's spine.
+def calculate_spine_angle(result: mp.tasks.vision.PoseLandmarkerResult):
+	# ASSUMPTION -- floor is completely level with camera
+	# TODO: detect floor plane, and account for that angle when calculating spine angle.
+	
+	
+	# trig goes here
+	print()
+
+
+# set model path
+model_path = 'models/pose_landmarker_heavy.task'
+# name is filename for mediapipe model
+model_name=model_path.split("/")[-1].split(".")[0]
+print("MODEL NAME IS "+model_name)
+
+# Create a pose landmarker instance with the video running mode:
 options = PoseLandmarkerOptions(
 		base_options=BaseOptions(model_asset_path=model_path),
 		running_mode=VisionRunningMode.VIDEO)
 
 start_time=datetime.now()
 
-with PoseLandmarker.create_from_options(options) as landmarker:
+# with landmarker:
+landmarker_model = PoseLandmarker.create_from_options(options)
 
-	# create cv2 video capture object
-	video_capture = cv2.VideoCapture('video.mp4')
-	while video_capture.isOpened() and (cv2.waitKey(25) & 0xFF != ord('q')):
-		# Capture frames.
-		success, image = video_capture.read()
-		# print error if no cap
-		if not success:
-			print("ERROR: Null.Frames")
-			break
+# create cv2 video capture object
+video_capture = cv2.VideoCapture('video.mp4')
+while video_capture.isOpened() and (cv2.waitKey(25) & 0xFF != ord('q')):
+	# Capture frames.
+	success, image = video_capture.read()
+	# print error if no cap
+	if not success:
+		print("ERROR: Null.Frames")
+		break
 
-		print("running feed")
+	# Get height and width of the frame.
+	# h, w = image.shape[:2]
 
-		# Get height and width of the frame.
-		h, w = image.shape[:2]
+	# convert cv2 image to mp image format
+	mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
+	# detect pose landmarks from the input image.
+	timestamp_ms = (int)((datetime.now()-start_time).total_seconds()*1000)
+	pose_landmarker_result = landmarker_model.detect_for_video(mp_image, timestamp_ms)
 
-		# convert cv2 image to mp image format
-		mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
-		# detect pose landmarks from the input image.
-		timestamp_ms = (int)((datetime.now()-start_time).total_seconds()*1000)
-		pose_landmarker_result = landmarker.detect_for_video(mp_image, timestamp_ms)
+	# use the generated PoseLandmarkerResult object:
+	# print(str(poseLandmarkerResult))
+	print(calculate_spine_angle(pose_landmarker_result))
 
-		# print resulting poseLandmarkerResult object
-		# print(str(poseLandmarkerResult))
+	# visualize the detection result:
 
-		# visualize the detection result:
+	annotated_image = draw_landmarks_on_image(image, pose_landmarker_result)
+	cv2.imshow(model_name,cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
 
-		annotated_image = draw_landmarks_on_image(image, pose_landmarker_result)
-		cv2.imshow("pose",cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
-
-
-
-		# cv2.imshow("window", image)
-		
-		# wait 1ms between frames
-		# rate is limited by webcam capture, not 1ms delay.
-		cv2.waitKey(1)
+	# wait 1ms between frames (not a bottleneck)
+	cv2.waitKey(1)
